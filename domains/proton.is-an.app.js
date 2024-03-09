25ba5a899507becb74ae11c032df8c3dcbc8bd8a
Belsicc <143416443+Belsicc@users.noreply.github.com>
2024-03-09 11:07:12 +0100

    Create proton.is-an.app.js

---
+++ domains/proton.is-an.app.js
@@ -0,0 +1,13 @@
+addSubDomain({
+  description: 'Personnal Project',
+  domain: 'is-an.app',
+  subdomain: 'Proton',
+  owner: {
+    repo: 'https://github.com/Belsicc/Maintenance-Proton',
+    email: 'yianis.nn@gmail.com',
+  },
+  record: {
+    CNAME: 'cname.vercel-dns.com',
+  },
+  proxy: false,
+})
